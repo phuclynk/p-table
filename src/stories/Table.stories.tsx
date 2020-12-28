87b24e371fb3252df1c6b8ea4ab6b356bff27c09
phuc tran <phuc.tran@fiisoft.com>
2020-12-28 16:35:44 +0700

    Init

---
+++ src/stories/Table.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { PaginatedTable } from '../components/table';
+
+export default {
+  title: 'Example/Table',
+  component: PaginatedTable,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta;
+
+const Template: Story<any> = (args) => <PaginatedTable {...args} />;
+
+export const Table = Template.bind({});
